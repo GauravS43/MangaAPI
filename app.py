373b9588d6055fba71ad15e54e709c4da2d91502
GauravS43 <gaurav53080@gmail.com>
2022-07-25 23:08:12 -0400

    added flask CORS

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from flask import *
+from flask_cors import CORS, cross_origin
 import json
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
 
 
 @app.route("/", methods=["GET"])
+@cross_origin()
 def index():
     with open("manga.json", "r") as f:
         return f.read()
 
 
 @app.route("/manga/", methods=["GET"])
+@cross_origin()
 def mangaRequest():
     manga_query = int(request.args.get("manga"))
 
